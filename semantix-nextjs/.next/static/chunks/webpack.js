cf256f2d8e30a3a650bcb81c40028e889011405b
shobian <s.shobian@gmail.com>
2023-10-07 20:11:40 +0530

    commit

--- semantix-nextjs/.next/static/chunks/webpack.js
+++ semantix-nextjs/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cb78d106475ac868"; }
+/******/ 		__webpack_require__.h = function() { return "67ac21bec549bcb1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- semantix-nextjs/.next/static/webpack/webpack.ac388d7fb09504b6.hot-update.js
+++ semantix-nextjs/.next/static/webpack/webpack.ac388d7fb09504b6.hot-update.js
@@ -33,7 +33,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "96f8051030c45c57"; }
+/******/ 	__webpack_require__.h = function() { return "d3f9bc58dc1ab74c"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
