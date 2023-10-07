36a2c34a909d9a41c16b61b7c75ffc7f11fffa4b
UmairShukri <123258035+UmairShukri@users.noreply.github.com>
2023-10-07 20:42:51 +0530

    sss

--- semantix-nextjs/.next/static/chunks/webpack.js
+++ semantix-nextjs/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fe1aac03c47d78d8"; }
+/******/ 		__webpack_require__.h = function() { return "576f143dd0284003"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
