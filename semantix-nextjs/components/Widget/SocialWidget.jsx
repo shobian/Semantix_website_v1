4ee1e53a83e7e209c3762854908928f1d6b5a5dc
UmairShukri <123258035+UmairShukri@users.noreply.github.com>
2023-10-10 00:37:28 +0530

    linkedin

--- semantix-nextjs/components/Widget/SocialWidget.jsx
+++ semantix-nextjs/components/Widget/SocialWidget.jsx
@@ -5,13 +5,13 @@ import Link from 'next/link';
 export default function SocialWidget() {
   return (
     <Div className="cs-social_btns cs-style1">
-      <Link href="/" className="cs-center">
+      <Link href="https://www.linkedin.com/company/semantix-labs/" className="cs-center">
         <Icon icon="fa6-brands:linkedin-in" />
       </Link>
       <Link href="https://www.instagram.com/semantixlabs/" className="cs-center">
         <Icon icon="fa6-brands:instagram" />
       </Link>
-      <Link href="/" className="cs-center">
+      <Link href="" className="cs-center">
         <Icon icon="fa6-brands:facebook" />
       </Link>
       <Link href="/" className="cs-center">
